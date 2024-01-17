326f26de3064e6f2eb2c8b38b706b7354f02c90e
guoguangtao <Jude_guo@163.com>
2024-01-17 17:53:40 +0800

    无限参数方法使用示例
    起因: 无
    结果: 无
    单号: 无
    
    Signed-off-by: ggt <guangtao_guo@lebo.cn>

--- app/src/main/java/com/example/androiddemo/MainActivity.java
+++ app/src/main/java/com/example/androiddemo/MainActivity.java
@@ -1,14 +1,32 @@
 package com.example.androiddemo;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.util.Log;
+
+import java.util.Arrays;
 
 public class MainActivity extends AppCompatActivity {
+
+    private final String TAG = "YXCMainActivity";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        infiniteParameterMethod("测试1", "1", "2", "3", "4");
+
+        infiniteParameterMethod("测试 2", "1");
+    }
+
+    private void infiniteParameterMethod(@NonNull String name, @NonNull Object... values) {
+
+        Log.d(TAG, name);
+        for (Object value : values) {
+            Log.d(TAG, value.toString());
+        }
     }
 }
